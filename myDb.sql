485536ee4646bdc2dc36a5b9835b3f6c7ab6a170
nathandrowley <50671362+nathandrowley@users.noreply.github.com>
2019-10-08 15:48:48 -0700

    Add files via upload
    mydbsql

---
+++ myDb.sql
@@ -0,0 +1,24 @@
+CREATE TABLE netflix (
+    ID SERIAL NOT NULL PRIMARY KEY,
+    media           varchar(80)   --movies and TV showes by name
+
+);
+
+CREATE TABLE hulu (
+    ID SERIAL NOT NULL PRIMARY KEY,
+    media           varchar(80)   --movies and TV showes by name
+
+);
+
+CREATE TABLE prime (
+    ID SERIAL NOT NULL PRIMARY KEY,
+    media           varchar(80)   --movies and TV showes by name
+
+);
+
+CREATE TABLE userList (
+    ID SERIAL NOT NULL PRIMARY KEY,
+    media           varchar(80)   --movies and TV showes by name
+
+);
+
