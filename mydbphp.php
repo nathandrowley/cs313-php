29b31e97151f17f544dd0e814f34d35056c989de
nathandrowley <50671362+nathandrowley@users.noreply.github.com>
2019-10-17 12:56:19 -0700

    Add files via upload
    DB php

---
+++ mydbphp.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>modern media guide</title>
+    <h1>Modern Media Guide</h1>
+</head>
+<body>
+​
+    <?php
+       try
+       {
+         $dbUrl = getenv('DATABASE_URL');
+
+         $dbOpts = parse_url($dbUrl);
+
+         $dbHost = $dbOpts["host"];
+         $dbPort = $dbOpts["port"];
+         $dbUser = $dbOpts["user"];
+         $dbPassword = $dbOpts["pass"];
+         $dbName = ltrim($dbOpts["path"],'/');
+
+         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       }
+       catch (PDOException $ex)
+       {
+         echo 'Error!: ' . $ex->getMessage();
+         die();
+       }
+​
+       foreach ($db->query('SELECT , media, content FROM netflix') as $row)
+       {
+         echo '<strong>' . $row['media'] . ' </strong>' . '"' . $row['content'] . '"' . '<br>';
+       }
+    ?>
+​
+</body>
+</html>
