fa29c7750d82b92005d747065b07d49390dc34c4
nathandrowley <50671362+nathandrowley@users.noreply.github.com>
2019-10-18 12:04:00 -0700

    Add files via upload
    without php

---
+++ datab.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>modern media guide</title>
+    <h1>Modern Media Guide</h1>
+</head>
+<body>
+
+<p>Testing testing testing</p>
+
+</body>
+</html>
