1de6d1f7747830652a806d10bb3eafe7b8629374
nathandrowley <50671362+nathandrowley@users.noreply.github.com>
2019-10-24 15:42:58 -0700

    Update myData.php
    updated H1.

--- web/myData.php
+++ web/myData.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <h1>Scripture Resources</h1>
+    <h1>My Media</h1>
 </head>
 <body>
 ​
